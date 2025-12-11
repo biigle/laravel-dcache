84625fe9f2ee58ab1c274b70994a2c86028e3abb
Martin Zurowietz <martin@zurowietz.de>
2025-12-11 09:10:05 +0100

    Add support for temporary URLs via macaroons

--- src/DCacheAdapter.php
+++ src/DCacheAdapter.php
@@ -8,10 +8,28 @@ class DCacheAdapter extends WebDAVAdapter
 {
     /**
      * Get a temporary URL for the file at the given path.
+     *
+     * See: https://dcache.org/manuals/UserGuide-10.2/webdav.shtml#requesting-macaroons
      */
     public function getTemporaryUrl($path, $expiration, $options): string
     {
-        // TODO: https://dcache.org/old/manuals/UserGuide-6.2/webdav.shtml#requesting-macaroons
-        return 'https://example.com';
+        $location = $this->encodePath($this->prefixer->prefixPath($path));
+        $url = $this->client->getAbsoluteUrl($location);
+
+        $response = $this->guzzle->request('POST', $url, [
+            'headers' => [
+                'Content-Type' => 'application/macaroon-request',
+            ],
+            'json' => [
+                'caveats' => [
+                    "activity:DOWNLOAD",
+                    "before:".$expiration->toIso8601ZuluString(),
+                ],
+            ],
+        ]);
+
+        $macaroon = json_decode($response->getBody());
+
+        return $macaroon->uri->targetWithMacaroon;
     }
 }
